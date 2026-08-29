13485154dcb6b10b96ed177627c908658a7b10a2
Ayush Mehta <ayushmehtabps@gmail.com>
2026-08-29 23:54:04 +0530

    tested server streaming

--- server/server_stream.go
+++ server/server_stream.go
@@ -7,17 +7,16 @@ import (
 	pb "github.com/ayushmehta03/grpc-golang/proto"
 )
 
-func (s*helloServer) SayHelloServerStream(req *pb.NamesList,stream pb.GreetService_SayHelloServerStreamingServer)error{
-	log.Printf("got request with names : %v",req.Names)
-
-	for _,name:= range req.Names{
-		res:=&pb.HelloResponse{
-			Message:"Hello"+name,
+func (s *helloServer) SayHelloServerStreaming(req *pb.NamesList, stream pb.GreetService_SayHelloServerStreamingServer) error {
+	log.Printf("Got request with names : %v", req.Names)
+	for _, name := range req.Names {
+		res := &pb.HelloResponse{
+			Message: "Hello " + name,
 		}
-		if err:=stream.Send(res);err!=nil{
+		if err := stream.Send(res); err != nil {
 			return err
 		}
-		time.Sleep(2*time.Second)
+		time.Sleep(2 * time.Second)
 	}
 	return nil
 }
