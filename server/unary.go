61d4924eb8c7e36672ffb72aab690e3a4b9c018f
Ayush Mehta <ayushmehtabps@gmail.com>
2026-08-29 22:58:04 +0530

    unary api for server

---
+++ server/unary.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"context"
+
+	pb "github.com/ayushmehta03/grpc-golang/proto"
+)
+
+//unary api-> single req and response model
+
+func (s *helloServer)sayHello(ctx context.Context, req *pb.NoParam)(*pb.HelloResponse,error){
+	return &pb.HelloResponse{
+		Message: "hey",
+	},nil
+}
